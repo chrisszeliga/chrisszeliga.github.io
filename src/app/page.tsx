cd7f4cf00dac2c6a9a787237b572ca1b862cbbc2
Chris Szeliga <christopherszeliga@gmail.com>
2025-11-30 01:37:23 -0600

    added fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 
-
 export default function Home() {
   return (
     <div>
-        
+      <p style={{ fontFamily: "GreatVibes-Regularbes" }}>Welcome to my portfolio!</p>
+      <p style={{ fontFamily: "Mondwest-Regular" }}>Welcome to my portfolio!</p>
+      <p style={{ fontFamily: "EditorialNew-Regular" }}>Welcome to my portfolio!</p>
     </div>
   );
 }
