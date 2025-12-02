f53beaa68488ab6f4f403027f1cba66e9837eb5a
Chris Szeliga <christopherszeliga@gmail.com>
2025-12-01 19:27:30 -0600

    Hero placeholder

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,19 @@
+import styles from './Hero.module.css'
+
+export default function Hero() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.body}>
+        <p><span>C</span>hristopher <span>S</span>zeliga</p>
+        <p>Software Engineer</p>
+        <p>big image</p>
+        <p>big image</p>
+        <p>big image</p>
+        <p>big image</p>
+        <p>big image</p>
+      </div>
+    </div>
+  )
+}
+
+// className={styles.pixel}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 'use client'
 import styles from './page.module.css'
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 export default function Home() {
   return (
-    <Header />
+    <main>
+      <Header />
+      <Hero />
+    </main>
   )
 }
